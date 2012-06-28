dda0ccf16eef9a9dafe4be13d49f404e05295dd6
David Joos <david.joos@gmail.com>
2012-06-28 21:06:19 +0200

    Supported OS

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,11 @@ description      "Installs/Configures CloudFuse"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-supports "ubuntu", "10.04"
+supports "ubuntu"
+supports "debian"
+supports "centos"
+supports "redhat"
+supports "fedora"
 
 depends "build-essential"
 depends "git"
